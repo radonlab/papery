ac32f3120b674064403d5037c4865045af87bf9c
skylerlee <skylerlee.me@gmail.com>
2016-06-16 13:03:09 +0800

    Added openshift entry

---
+++ wsgi.py
@@ -0,0 +1,10 @@
+#!/usr/bin/env python
+"""
+Copyright (C) 2015, Radmon.
+Use of this source code is governed by the MIT license that can be
+found in the LICENSE file.
+"""
+
+from papery import create_app
+
+application = create_app()
